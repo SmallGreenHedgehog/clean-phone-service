f9965f6ee9a6b4baea9097d3d03e1bf976211ec7
mr_v <v.about.work@gmail.com>
2022-01-14 13:31:32 +0300

    Добавит отладочную страницу echo_request

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from typing import Type
 from flask import Flask
+from flask import request
 from preparers_for_phone_number import PhoneNumberPreparer, PhoneNumberPlusSevenNinePreparer
 
 app = Flask(__name__)
@@ -19,7 +20,13 @@ def prepare_russian_phone(phone: str):
 @app.route('/api/v1/single-phone/', methods=['GET'])
 @app.route('/api/v1/single-phone/<string:phone>', methods=['GET'])
 def single_phone_page(phone=''):
+    """Возвращает подготовленный номер телефона на базе переданного"""
     return prepare_russian_phone(phone)
+
+@app.route('/api/v1/echo-request/', methods=['POST'])
+def echo_request_page():
+    """Возвращает данные запроса, отладочная страница"""
+    return request.data
 
 
 if __name__ == '__main__':
